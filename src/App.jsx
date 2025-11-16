24262bf8f3680425f567220f995a4bc82984b65f
Andrew Porto <andrewluizporto@hotmail.com>
2025-11-16 19:57:22 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+   <div className='flex-center h-[100vh]'>
+    <h1 className='text-4xl font-bold text-white'>Hello Gsap</h1>
+   </div>
+  )
+}
